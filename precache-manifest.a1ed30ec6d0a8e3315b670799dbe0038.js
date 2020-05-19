bd03a490514827a40baf668c14d2a98689628411
Sanghyun Choi <sanghyunc7@gmail.com>
2020-05-18 21:33:15 -0400

    Updates

---
+++ precache-manifest.a1ed30ec6d0a8e3315b670799dbe0038.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f4270b2ae9a8625bfa97f57a5b8310",
+    "url": "/my-app1/index.html"
+  },
+  {
+    "revision": "68b076f6e4d084ede8ae",
+    "url": "/my-app1/static/css/main.80f4ae35.chunk.css"
+  },
+  {
+    "revision": "d54bbfa4ae22539bd0f0",
+    "url": "/my-app1/static/js/2.841607f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app1/static/js/2.841607f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b076f6e4d084ede8ae",
+    "url": "/my-app1/static/js/main.3108f0bf.chunk.js"
+  },
+  {
+    "revision": "cdbe823742b72e6d5afb",
+    "url": "/my-app1/static/js/runtime-main.acfa5fa7.js"
+  },
+  {
+    "revision": "78cabc215ec67c8789070aefa9bacc00",
+    "url": "/my-app1/static/media/logo.78cabc21.jpg"
+  }
+]);
